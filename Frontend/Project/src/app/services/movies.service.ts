73d6efa0be9e109ea3fd96913e7046ec964c6d3e
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-19 15:55:23 +0500

    created models interfaces for movie and cinema(it is temporarily, i will change after backend developers create api)

--- Frontend/Project/src/app/services/movies.service.ts
+++ Frontend/Project/src/app/services/movies.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  private apiUrl = ''
 
-  constructor() { }
+
+  constructor(private client: HttpClient) { }
+
+  getAllMovies(): Observable<Movie[]> {
+    return this.client.get<Movie[]>(this.apiUrl);
+  }
+  getMovieById(id: number): Observable<Movie> {
+    return this.client.get<Movie>(`${this.apiUrl}${id}/`);
+  }
 }
---
+++ Frontend/Project/src/models.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+    id: number;
+    title: string;
+    description: string;
+    genre: string;
+    release_year: number;
+    image_url: string;
+}
+
+export interface Cinema {
+    id: number;
+    name: string;
+    location: string;
+    
+}
