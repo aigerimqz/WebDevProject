fe6d90538512cb92b61410c8e9cecd32ec4b653c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-19 17:16:19 +0500

    generated movie-detail component

--- Frontend/Project/src/app/app.routes.ts
+++ Frontend/Project/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { CinemasComponent } from './cinemas/cinemas.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'movies', component: MoviesComponent},
-    {path: 'cinemas', component: CinemasComponent}
+    {path: 'cinemas', component: CinemasComponent},
+    {path: 'movies/:id', component: MovieDetailComponent}
 ];
