535b89c5b70ed2d18ab634ee3ca654e90fd2dc83
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-19 13:42:24 +0500

    modified app.routes

--- Frontend/Project/src/app/app.routes.ts
+++ Frontend/Project/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { CinemasComponent } from './cinemas/cinemas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'movies', component: MoviesComponent},
+    {path: 'cinemas', component: CinemasComponent}
+];
