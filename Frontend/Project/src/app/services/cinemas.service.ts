20f5a45a078affad656ca74ec5f986a795e428c8
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-19 18:27:14 +0500

    added cinema service functions

--- Frontend/Project/src/app/services/cinemas.service.ts
+++ Frontend/Project/src/app/services/cinemas.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Cinema } from '../../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CinemasService {
+  private cinemas: Cinema[] = [
+    {
+      id: 1,
+      name: "Lumeria Cinema",
+      location: "Abylaikhan 62"
+    },
+    {
+      id: 2,
+      name: "Chaplin Mega Park",
+      location: "Makatayev 21"
+    }
+  ]
 
   constructor() { }
+
+  getAllCinemas(): Cinema[]{
+    return this.cinemas;
+  }
+  getCinemaById(id: number): Cinema | undefined {
+    return this.cinemas.find(cinema => cinema.id === id);
+  }
 }
