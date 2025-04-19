ad9d59bc22f6b25977f41f589986a9a06e3b6d79
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-19 16:03:50 +0500

    just for example i create  part of json in movies comp

--- Frontend/Project/src/app/movies/movies.component.ts
+++ Frontend/Project/src/app/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from '../../models';
 
 @Component({
   selector: 'app-movies',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './movies.component.css'
 })
 export class MoviesComponent {
+  //just for example i create this part of json 
+  movies: Movie[] = [
+    {
+      id: 1,
+      title: "Captain America: Brave New World",
+      description: "Новый президент США Таддеус Росс решает назначить Капитана Америку на официальную должность. Но в прошлом между ними были разногласия, и Сэм сомневается, что это хорошая идея. Этим же вечером на президента совершается покушение. Уилсон уверен, что предатель скрывается среди ближайшего окружения Росса.",
+      genre: 'action',
+      release_year: 2025,
+      image_url: 'https://m.media-amazon.com/images/M/MV5BNDRjY2E0ZmEtN2QwNi00NTEwLWI3MWItODNkMGYwYWFjNGE0XkEyXkFqcGc@._V1_.jpg'
+
+    }
+  ]
 
 }
