d4eea3c82c2b9929f934f8a4421cbedf7ce252cf
yasmin <taylorishere@mail.ru>
2025-04-23 00:23:45 +0500

    changed movie model

--- Backend/environment/Scripts/cinemas_back/api/models.py
+++ Backend/environment/Scripts/cinemas_back/api/models.py
@@ -9,9 +9,24 @@ class Cinema(models.Model):
         return self.name
 
 class Movies(models.Model):
+    #id = models.IntegerField()
     title = models.CharField(max_length=255)
     description = models.TextField()
+    genre = models.CharField(max_length=255)
     release_year = models.IntegerField()
+    release_date = models.IntegerField()
+    duration = models.PositiveIntegerField()
+    image_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
+    
+class Screening(models.Model):
+    movieId = models.IntegerField()
+    cinemaId = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.movie.title} at {self.datetime} in {self.cinema.name}"
