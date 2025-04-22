b9eddfc7cbf1a956f300c697ce28f6f6683e5aab
Aya <macbook@Noutbuk-Macbook-2.local>
2025-04-23 00:23:24 +0500

    added screeningsmodel

--- Backend/environment/Scripts/cinemas_back/api/models.py
+++ Backend/environment/Scripts/cinemas_back/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Cinema(models.Model):
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    
 
     def __str__(self):
         return self.name
@@ -15,3 +15,13 @@ class Movies(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Screening(models.Model):
+    movie = models.ForeignKey('Movies', on_delete=models.CASCADE)
+    cinema = models.ForeignKey('Cinema', on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.movie.title} at {self.cinema.name} on {self.date} {self.time}"
