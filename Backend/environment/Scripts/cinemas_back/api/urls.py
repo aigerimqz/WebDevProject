99566d3b4f8e636d75f6ffc36dd05dff66c4aec2
yasmin <taylorishere@mail.ru>
2025-04-23 12:51:58 +0500

    trying to add auth

--- Backend/environment/Scripts/cinemas_back/api/urls.py
+++ Backend/environment/Scripts/cinemas_back/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 # from .views import CinemaListCreateView, CinemaDetailView, MovieListCreateView
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    #path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('cinema/', CinemaListCreateView.as_view()),
     path('cinema/<int:pk>/', CinemaDetailView.as_view()),
     path('movies/', MovieListCreateView),
--- Backend/environment/Scripts/cinemas_back/cinemas_back/settings.py
+++ Backend/environment/Scripts/cinemas_back/cinemas_back/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'corsheaders',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Internationalization
