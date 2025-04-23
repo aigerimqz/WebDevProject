6ab208f288d669caba63fb0b3c891b07e6c1141d
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-23 13:38:52 +0500

    generated auth service, and added login and logout logics

--- Frontend/Project/src/app/auth/auth.component.ts
+++ Frontend/Project/src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 // import { User } from '../../models';
 
 @Component({
@@ -12,6 +13,23 @@ import { RouterModule } from '@angular/router';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  onLogin(){
+    const success  = this.authService.login(this.email, this.password);
+    if(success){
+      this.router.navigate(['/']);
+    }else{
+      this.errorMessage = 'Wrong email or password';
+    }
+  }
   
 
 
